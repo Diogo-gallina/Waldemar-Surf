2617597d72bd459a3d0118891c78747c3246cfc5
Diogo-gallina <diogogallina20@gmail.com>
2023-12-04 18:49:34 -0300

    feat: add try cath on fetchPoints method

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -38,6 +38,7 @@ export class StormGlass {
   constructor(protected request: AxiosStatic) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    try{
     const response = await this.request.get<StormGlassForecastResponse>(
       `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
       {
@@ -47,6 +48,9 @@ export class StormGlass {
       }
     );
     return this.normalizeResponse(response.data);
+    } catch(err) {
+      throw new Error('Unexpected error when trying to communicate to StormGlass: Network Error');
+    }
   }
 
   private normalizeResponse(
