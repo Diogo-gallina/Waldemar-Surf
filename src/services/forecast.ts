e1ad7432492aad27976b894b482587b83c86de45
Diogo-gallina <diogogallina20@gmail.com>
2023-12-10 11:04:20 -0300

    chore: chonge position wrong name

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -9,14 +9,29 @@ export enum BeachPosition {
 
 export interface Beach {
     name: string;
-    positiron: BeachPosition;
+    position: BeachPosition;
     lat: number;
     lng: number;
     user: string;
 }
 
 export class Forecast {
-    constructor(protected stormGlass: StormGlass()){}
+    constructor(protected stormGlass = StormGlass()) {}
 
-    public async processForecastForBeach(beaches: )
+    public async processForecastForBeach(beaches: Beach[]): Promise<[]> {
+        const pointsWithCorrectSources = [];
+        for(const beach of beaches){
+            const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+            const enrichedBeachData = points.map((e) => ({
+                ...{
+                    lat: beach.lat,
+                    lng: beach.lng,
+                    name: beach.name,
+                    position: beach.position,
+                    rating: 1
+                },
+                ...e,
+            }))
+        }
+    }
 }
