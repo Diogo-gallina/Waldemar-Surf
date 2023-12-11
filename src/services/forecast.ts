b3953ff62949e8ab9fbc0378fc9d75865b94be97
Diogo-gallina <diogogallina20@gmail.com>
2023-12-11 08:29:43 -0300

    feat: create beach forecast interface

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,37 +1,42 @@
-import { StormGlass } from '@src/clients/stormGlass'
+import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
 
 export enum BeachPosition {
-    S = 'S',
-    E = 'E',
-    W = 'W',
-    N = 'N'
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
 }
 
 export interface Beach {
-    name: string;
-    position: BeachPosition;
-    lat: number;
-    lng: number;
-    user: string;
+  name: string;
+  position: BeachPosition;
+  lat: number;
+  lng: number;
+  user: string;
 }
 
-export class Forecast {
-    constructor(protected stormGlass = StormGlass()) {}
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
-    public async processForecastForBeach(beaches: Beach[]): Promise<[]> {
-        const pointsWithCorrectSources = [];
-        for(const beach of beaches){
-            const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-            const enrichedBeachData = points.map((e) => ({
-                ...{
-                    lat: beach.lat,
-                    lng: beach.lng,
-                    name: beach.name,
-                    position: beach.position,
-                    rating: 1
-                },
-                ...e,
-            }))
-        }
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeach(
+    beaches: Beach[]
+  ): Promise<BeachForecast[]> {
+    const pointsWithCorrectSources: BeachForecast[] = [];
+    for (const beach of beaches) {
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = points.map((e) => ({
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1,
+        },
+        ...e,
+      }));
+      pointsWithCorrectSources.push(...enrichedBeachData);
     }
+  }
 }
