709fd9262d34bce4db2aaf22682c7036e0da9c17
Diogo-gallina <diogogallina20@gmail.com>
2023-12-11 21:39:47 -0300

    feat: create database setup method

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import './util/module-alias.util';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast.controller';
 import { Application } from 'express';
+import * as database from '@src/database'
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -21,6 +23,10 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
   }
 
   public getApp(): Application {
