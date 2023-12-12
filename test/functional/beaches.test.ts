c41bc3e7e8ae395f5f57360d5d48d10bf939ca9f
Diogo-gallina <diogogallina20@gmail.com>
2023-12-12 10:42:27 -0300

    test: create error validation beach test

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -16,5 +16,21 @@ describe('Beaches functional tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.statusCode).toBe(422);
+      expect(response.body).toEqual({
+        error: 
+        'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+      });
+    });
   });
 });
