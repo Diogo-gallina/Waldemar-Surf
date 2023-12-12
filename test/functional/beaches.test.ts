86df2b7760ac12b9b8cfe356e79219d2f81d60d0
Diogo-gallina <diogogallina20@gmail.com>
2023-12-12 10:09:12 -0300

    fix: change send method

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -8,7 +8,7 @@ describe('Beaches functional tests', () => {
         position: 'E',
       };
 
-      const response = (await global.testRequest.post('/beaches')).setEncoding(newBeach);
+      const response = await global.testRequest.post('/beaches').send(newBeach);
       expect(response.status).toBe(201);
       expect(response.body).toEqual(newBeach);
     });
