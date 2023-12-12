cc023c7e256ad4f49619579490368bae5f994fcc
Diogo-gallina <diogogallina20@gmail.com>
2023-12-12 15:27:48 -0300

    test: create internal error test

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,6 +1,6 @@
 import { Beach, BeachPosition } from '@src/models/beach.model';
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
-import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json'
+import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json';
 import nock from 'nock';
 
 describe('Beach forecast functional tests', () => {
@@ -36,5 +36,24 @@ describe('Beach forecast functional tests', () => {
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
     expect(body).toEqual(apiForecastResponse1BeachFixture);
+  });
+
+  it('should return 500 if something goes wrong during the processing', async () => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat: '-33.792726',
+        lng: '151.289824',
+      })
+      .replyWithError('Something went wrong');
+
+      const { status } = await global.testRequest.get('/forecast');
+      expect(status).toBe(500)
   });
 });
