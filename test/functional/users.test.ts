1b554d97997191598dbb2e6de3702d8258e409c4
Diogo-gallina <diogogallina20@gmail.com>
2023-12-14 17:20:02 -0300

    test: throw 400 when there is a validation error

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -4,7 +4,7 @@ describe('Users functional tests', () => {
   beforeEach(async () => {
     await User.deleteMany();
   });
-  
+
   describe('When creating a new user', () => {
     it('should successfuly create a new user', async () => {
       const newUser = {
@@ -17,6 +17,21 @@ describe('Users functional tests', () => {
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+
+    it('should throw 400 when there is a validation error', async () => {
+      const newUser = {
+        email: 'john@mail.com',
+        password: '1234',
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        code: 400,
+        error: 'User validation faled: name: Path `name` is required.',
+      });
     });
   });
 });
