1486936a9856a007b4b5227988967ad14fd825d9
Diogo-gallina <diogogallina20@gmail.com>
2023-12-14 10:34:18 -0300

    chore: error message

--- src/controllers/forecast.controller.ts
+++ src/controllers/forecast.controller.ts
@@ -1,19 +1,23 @@
 import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach.model';
 import { ForecastService } from '@src/services/forecast.service';
-import { Request, Response } from 'express';
 
-const forecastService = new ForecastService()
+const forecastService = new ForecastService();
+
 @Controller('forecast')
 export class ForecastController {
   @Get('')
-  public async getForecastForLoggedUser(_: Request, res: Response): Promise<void> {
+  public async getForecastForgeLoggedUser(
+    _: Request,
+    res: Response
+  ): Promise<void> {
     try {
-    const beaches = await Beach.find({})
-    const forecastData = await forecastService.processForecastForBeach(beaches);
-    res.status(200).send(forecastData);
-    } catch (err) {
-      res.status(500).send({ error: 'Something went wrong'});
+      const beaches = await Beach.find({});
+      const forecastData = await forecastService.processForecastForBeach(beaches);
+      res.status(200).send(forecastData);
+    } catch (error) {
+      res.status(500).send({ error: 'Something went wrong' });
     }
   }
 }
