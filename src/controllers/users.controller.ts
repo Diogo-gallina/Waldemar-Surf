fe49922055433181ea200000a4a1eb225273d3ca
Diogo-gallina <diogogallina20@gmail.com>
2023-12-19 17:33:56 -0300

    feat: use sendCreateUpdateErrorResponse method in user controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user.model';
 import { Request, Response } from 'express';
+import { BaseController } from './index';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
     try {
@@ -11,7 +12,7 @@ export class UsersController {
       const newUser = await user.save();
       res.status(201).send(newUser);
     } catch (error) {
-      res.status(400).send({ error: error.message });
+      this.sendCreateUpdateErrorResponse(res, error);
     }
   }
 }
