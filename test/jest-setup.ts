1ee62c406f8d112e5275b30bf754900badb8b3a3
Diogo-gallina <diogogallina20@gmail.com>
2023-12-20 15:16:23 -0300

    feat: add jest timeout

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -2,10 +2,14 @@ import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
 let server: SetupServer;
+
 beforeAll(async () => {
+  jest.setTimeout(10000);
   server = new SetupServer();
   await server.init();
   global.testRequest = supertest(server.getApp());
 });
 
-afterAll(async () => await server.close());
+afterAll(async () => {
+  await server.close();
+});
