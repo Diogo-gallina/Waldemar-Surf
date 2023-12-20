080fc7dedcecd52304d8ba8e0335a5497767c659
Diogo-gallina <diogogallina20@gmail.com>
2023-12-20 10:12:10 -0300

    feat: create handleClientErrors method

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_VALIDATION } from '@src/models/user.model';
 import { Response } from 'express';
 import mongoose from 'mongoose';
 
@@ -7,9 +8,21 @@ export abstract class BaseController {
     error: mongoose.Error.ValidationError | Error
   ): void {
     if (error instanceof mongoose.Error.ValidationError) {
-      res.status(422).send({ code: 422, error: error.message });
     } else {
       res.status(500).send({ code: 500, error: 'Soomething went wrong' });
     }
+  }
+
+  private handleClientErrors(error: mongoose.Error.ValidationError): {
+    code: number;
+    error: string;
+  } {
+    const duplicatedKindErrors = Object.values(error.errors).filter(
+      (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
+    );
+    if (duplicatedKindErrors.length) {
+      return { code: 409, error: error.message };
+    }
+    return { code: 422, error: error.message };
   }
 }
