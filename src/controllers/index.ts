0f8700a47ac72a5fc03d9e372b40f2003bcc79c3
Diogo-gallina <diogogallina20@gmail.com>
2023-12-20 10:24:07 -0300

    fix: change error type

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,17 +3,14 @@ import { Response } from 'express';
 import mongoose from 'mongoose';
 
 export abstract class BaseController {
-  protected sendCreateUpdateErrorResponse(
-    res: Response,
-    error: mongoose.Error.ValidationError | Error
-  ): void {
+  protected sendCreateUpdateErrorResponse(res: Response, error: unknown): void {
     if (error instanceof mongoose.Error.ValidationError) {
       const clientErrors = this.handleClientErrors(error);
       res
         .status(clientErrors.code)
         .send({ code: clientErrors.code, error: clientErrors.error });
     } else {
-      res.status(500).send({ code: 500, error: 'Soomething went wrong' });
+      res.status(500).send({ code: 500, error: 'Something went wrong!' });
     }
   }
 
@@ -22,7 +19,9 @@ export abstract class BaseController {
     error: string;
   } {
     const duplicatedKindErrors = Object.values(error.errors).filter(
-      (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
+      (err) =>
+        err.name === 'ValidatorError' &&
+        err.kind === CUSTOM_VALIDATION.DUPLICATED
     );
     if (duplicatedKindErrors.length) {
       return { code: 409, error: error.message };
