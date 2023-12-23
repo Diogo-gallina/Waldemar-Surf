96976f8d8d57a47c13c097808b36b590a79b217c
Diogo-gallina <diogogallina20@gmail.com>
2023-12-23 16:49:28 -0300

    chore:  change optional params

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,11 +2,11 @@ import AuthService from '@src/services/auth.service';
 import { NextFunction, Request, Response } from 'express';
 
 export function authMiddleware(
-  req: Request,
-  _: Response,
+  req: Partial<Request>,
+  _: Partial<Response>,
   next: NextFunction
 ): void {
-  const token = req.headers['x-access-token'];
+  const token = req.headers?.['x-access-token'];
   const decoded = AuthService.decodeToken(token as string);
   req.decoded = decoded;
   next();
