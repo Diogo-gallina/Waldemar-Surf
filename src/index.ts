b7dd8519c11107ead932a2ce1c7dc8f41df2ce2e
Diogo-gallina <diogogallina20@gmail.com>
2024-01-12 09:50:01 -0300

    config: add log in initial server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import logger from './logger';
 import { SetupServer } from './server';
 import config from 'config';
 
 (async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'));
-  await server.init();
-  server.start();
+  try {
+    const server = new SetupServer(config.get('App.port'));
+    await server.init();
+    server.start();
+  } catch (error) {
+    logger.error('App exited with error: ' + error);
+    process.exit(1);
+  }
 })();
