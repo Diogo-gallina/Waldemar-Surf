bd7aa2f69e09f0eb49943f81921ad478a12afbf6
Diogo-gallina <diogogallina20@gmail.com>
2024-01-25 23:14:36 -0300

    chore: format parameter name

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -21,9 +21,9 @@ export abstract class BaseController {
     error: string;
   } {
     const duplicatedKindErrors = Object.values(error.errors).filter(
-      (err) =>
-        err.name === 'ValidatorError' &&
-        err.kind === CUSTOM_VALIDATION.DUPLICATED
+      (error) =>
+      error.name === 'ValidatorError' &&
+      error.kind === CUSTOM_VALIDATION.DUPLICATED
     );
     if (duplicatedKindErrors.length) {
       return { code: 409, error: error.message };
