2c2b8cdd801f9c4edde04f3f78023ba1c4af5d75
Diogo-gallina <diogogallina20@gmail.com>
2024-02-02 09:17:17 -0300

    chore: remove unused logger

--- src/controllers/forecast.controller.ts
+++ src/controllers/forecast.controller.ts
@@ -4,7 +4,6 @@ import { Beach } from '@src/models/beach.model';
 import { ForecastService } from '@src/services/forecast.service';
 import { authMiddleware } from '@src/middlewares/auth.middleware';
 import { BaseController } from './index';
-import logger from '@src/logger';
 
 const forecastService = new ForecastService();
 
@@ -22,7 +21,6 @@ export class ForecastController extends BaseController {
         await forecastService.processForecastForBeach(beaches);
       res.status(200).send(forecastData);
     } catch (error) {
-      logger.error(error);
       this.sendErrorResponse(res, {
         code: 500,
         message: 'Something went wrong',
