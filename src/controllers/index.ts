6645234d50bd250164c70a6d8d21e55c9ec5db82
Diogo-gallina <diogogallina20@gmail.com>
2024-03-02 23:57:50 -0300

    chore: lint index ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,26 +5,31 @@ import mongoose from 'mongoose';
 import ApiError, { APIError } from '@src/util/errors/api-error';
 
 export abstract class BaseController {
-  protected sendCreateUpdateErrorResponse(res: Response, error: unknown): void {
+  protected sendCreateUpdateErrorResponse(
+    res: Response,
+    error: unknown
+  ): void {
     if (error instanceof mongoose.Error.ValidationError) {
       const clientErrors = this.handleClientErrors(error);
-      res
-        .status(clientErrors.code)
-        .send(ApiError.format({ code: clientErrors.code, message: clientErrors.error }));
+      res.status(clientErrors.code).send(
+        ApiError.format({
+          code: clientErrors.code,
+          message: clientErrors.error,
+        })
+      );
     } else {
-      logger.error(error);
-      res.status(500).send(ApiError.format({ code: 500, message: 'Something went wrong!' }));
+      logger.error(JSON.stringify(error));
+      res
+        .status(500)
+        .send(ApiError.format({ code: 500, message: 'Something went wrong!' }));
     }
   }
 
-  private handleClientErrors(error: mongoose.Error.ValidationError): {
-    code: number;
-    error: string;
-  } {
+  private handleClientErrors(
+    error: mongoose.Error.ValidationError
+  ): { code: number; error: string } {
     const duplicatedKindErrors = Object.values(error.errors).filter(
-      (err) =>
-        err.name === 'ValidatorError' &&
-        err.kind === CUSTOM_VALIDATION.DUPLICATED
+      (err) => err.name === 'ValidatorError' && err.kind === CUSTOM_VALIDATION.DUPLICATED
     );
     if (duplicatedKindErrors.length) {
       return { code: 409, error: error.message };
